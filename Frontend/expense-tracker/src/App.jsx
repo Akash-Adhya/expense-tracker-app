fea88900be21e7428ccc44d73e2313cad5ff4f53
Akash Adhya <90973800+Akash-Adhya@users.noreply.github.com>
2025-11-15 19:01:50 +0530

    freeing the clutter

--- Frontend/expense-tracker/src/App.jsx
+++ Frontend/expense-tracker/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import Login from "./pages/Login";
-import { SignUp } from "@clerk/clerk-react";
+import Signup from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<Signup />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
--- Frontend/expense-tracker/src/main.jsx
+++ Frontend/expense-tracker/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { AppProvider } from "./context/AppContext";
+
 import App from "./App";
 import "./index.css";
 
@@ -10,13 +9,8 @@ const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider
-      publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
